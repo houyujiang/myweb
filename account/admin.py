7223ad93fefd0808014ddedf9f73e252551bc5a6
houyu <houyujiang@live.cn>
2018-02-12 02:37:30 +0800

    添加用户上传头像功能

--- account/admin.py
+++ account/admin.py
@@ -2,11 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile,UserInfo
 # Register your models here.
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user','birth','phone')
     list_filter = ('phone',)
-
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('user','school','company','address','profession','aboutme','photo')
+    list_filter = ('school','company','profession')
 admin.site.register(UserProfile,UserProfileAdmin)
+admin.site.register(UserInfo,UserInfoAdmin)
